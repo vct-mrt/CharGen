730df43c04921d9b29a5db5b93b2f4a687598da9
vct-mrt <victor.martinetti@epitech.eu>
2024-12-16 16:00:31 +0100

    [add]: my getnbr

--- src/process.c
+++ src/process.c
@@ -15,6 +15,20 @@ static int my_strlen(char *nbr)
     return i;
 }
 
+static int my_getnbr(char *nbr)
+{
+    int nb = 0;
+    int i = 0;
+
+    while (nbr[i + 1] != '\0') {
+        nb += ((int)nbr[i] - 48);
+        nb *= 10;
+        i++;
+    }
+    nb += ((int)nbr[i] - 48);
+    return nb;
+}
+
 int process(char *nbr)
 {
     char alpha[] = "abcdefghijklmnopqrs tuvwxyz0123456789 ";
