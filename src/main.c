c18da007e11fb1afa081b546590197400cba74b2
vim <victor.martinetti@techup.fr>
2024-12-16 15:58:12 +0100

    [add]: -h to main

--- src/main.c
+++ src/main.c
@@ -5,11 +5,12 @@ int main(int ac, char **av)
     if (ac == 1 || ac > 2) {
         flag_help();
         return 84;
-    }
-    else if (error(ac, av) == 84) {
+    } else if (error(ac, av) == 84) {
         flag_help();
         return 84;
-    }
-    else
+    } else if (ac == 2 && av[1][0] == '-' && av[1][0] == 'h') {
+        flag_help();
+        return 0;
+    } else
         return process(av[1]);
 }
