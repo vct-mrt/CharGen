fdbf97a444300eb6eb28b34221cfddada9338d59
vct-mrt <victor.martinetti@epitech.eu>
2024-12-16 17:21:09 +0100

    [add]: gestion overflow

--- include/random_char.h
+++ include/random_char.h
@@ -4,10 +4,13 @@
     #include <string.h>
     #include <stdio.h>
     #include <time.h>
+    #include <limits.h>
 
-
-int process(char *nbr);
 void flag_help(void);
+int my_random(int nb);
+int process(char *nbr);
+int my_getnbr(char *nbr);
+int my_strlen(char *nbr);
 int error(int ac, char **av);
 
 #endif //RANDOM_CHAR
--- src/process.c
+++ src/process.c
@@ -1,34 +1,5 @@
 #include "random_char.h"
 
-static int my_random(int nb)
-{
-    return rand() % nb;
-}
-
-static int my_strlen(char *nbr)
-{
-    int i = 0;
-
-    while (nbr[i] != '\0') {
-        i++;
-    }
-    return i;
-}
-
-static int my_getnbr(char *nbr)
-{
-    int nb = 0;
-    int i = 0;
-
-    while (nbr[i + 1] != '\0') {
-        nb += ((int)nbr[i] - 48);
-        nb *= 10;
-        i++;
-    }
-    nb += ((int)nbr[i] - 48);
-    return nb;
-}
-
 int process(char *nbr)
 {
     char alpha[] = "abcdefghijklmnopqrstuvwxyz0123456789 ";
@@ -36,15 +7,16 @@ int process(char *nbr)
     int ind = my_getnbr(nbr);
     int rand = -1;
     int len = my_strlen(alpha);
+    printf("nb = %d\n", ind);
 
-    srand(time(NULL));
-    while (i < ind) {
-        rand = my_random(len);
-        printf("%c", alpha[rand]);
-        if (i != 0 && i % 100 == 0)
-            printf("\n");
-        i++;
-    }
-    printf("\n");
+    //srand(time(NULL));
+    //while (i < ind) {
+    //    rand = my_random(len);
+    //    printf("%c", alpha[rand]);
+    //    if (i != 0 && i % 100 == 0)
+    //        printf("\n");
+    //    i++;
+    //}
+    //printf("\n");
     return 0;
 }
