a73c7ed5d519e360ccb0a68b1f17c45da70f61bc
vct-mrt <victor.martinetti@epitech.eu>
2024-12-17 09:58:20 +0100

    [update]: -h for the news feature

--- src/flag_help.c
+++ src/flag_help.c
@@ -2,8 +2,11 @@
 
 void flag_help(void)
 {
-    printf("Usage: ./my_random_char <number>\n");
+    printf("Usage: ./my_random_char <options> <number>\n");
     printf("Options:\n");
     printf("\t-h\t\tDisplay this help message\n");
-    printf("\t<number>\tProcess the given file\n");
+    printf("\t-a\t\tgenerates only numeric\n");
+    printf("\t<number>\tnumber of characters to generate\n");
+    printf("By default :\n");
+    printf("\t[./my_random_char <number>] generates alpha numeric with special characters\n");
 }
