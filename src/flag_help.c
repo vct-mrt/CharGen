4f416e267ddf4da465b6a2165d6aa2697416f4b3
vct-mrt <victor.martinetti@epitech.eu>
2024-12-17 14:21:35 +0100

    [add]: start -c

--- src/flag_help.c
+++ src/flag_help.c
@@ -6,6 +6,7 @@ int flag_help(void)
     printf("Options:\n");
     printf("\t-h\t\tDisplay this help message\n");
     printf("\t-a\t\tgenerates only numeric\n");
+    printf("\t-c\t\tgenerates only letters of the alphabet\n");
     printf("\t<number>\tnumber of characters to generate\n");
     printf("By default :\n");
     printf("\t[./my_random_char <number>] generates alpha numeric with special characters\n");
