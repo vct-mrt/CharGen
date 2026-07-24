ec93528afe81f9ed1c7d2958a0c3c7cc24c8c74d
vct-mrt <martinetti.victor11@gmail.com>
2026-07-24 13:21:27 +0200

    bump version to 1.0.4 in random_char.h

--- include/random_char.h
+++ include/random_char.h
@@ -8,7 +8,7 @@
     #include <sys/types.h>
     #include <limits.h>
     #include <stdbool.h>
-    #define VERSION "1.0.3"
+    #define VERSION "1.0.4"
     #define LINE_WIDTH 100
     #define ALPHA_MIN "abcdefghijklmnopqrstuvwxyz"
     #define ALPHA_MAJ "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
